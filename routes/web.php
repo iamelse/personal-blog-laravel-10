d04f5dc4f2398bbee80883ad4036fb33538ab735
Lana <lana.septiana2@gmail.com>
2024-02-07 09:47:07 +0700

    Patch * feat: block unautheticate user from accessing dashboard * feat: redirect authenticated user to dashboard

--- routes/web.php
+++ routes/web.php
@@ -43,8 +43,10 @@ Route::get('/a-post', function () {
     return view('frontend.a-post');
 });
 
-Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('/authenticate', [LoginController::class, 'authenticate'])->name('authenticate');
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login')->middleware('guest');
+Route::post('/authenticate', [LoginController::class, 'authenticate'])->name('authenticate')->middleware('guest');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::get('/dashboard', [DashboardController::class, 'showPage'])->name('dashboard');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/dashboard', [DashboardController::class, 'showPage'])->name('dashboard');
+});
