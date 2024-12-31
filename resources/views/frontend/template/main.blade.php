28407a8838c152d8e06fd0da4ab1b4fe4c8f782b
Lana Septiana <lana.septiana2@gmail.com>
2024-12-31 11:09:39 +0700

    feat(navbar): add smooth transition for shadow effect on scroll

--- resources/views/frontend/template/main.blade.php
+++ resources/views/frontend/template/main.blade.php
@@ -23,6 +23,18 @@
         });
     </script>
 
+    <script>
+        window.addEventListener('scroll', function() {
+            const navbar = document.querySelector('.navbar');
+            
+            if (window.scrollY > 10) {
+                navbar.classList.add('shadow-sm');
+            } else {
+                navbar.classList.remove('shadow-sm');
+            }
+        });
+    </script>
+
 </body>
 
 </html>
--- storage/framework/views/7a3c0b487762762783d85be4dd9e701d.php
+++ storage/framework/views/7a3c0b487762762783d85be4dd9e701d.php
@@ -23,6 +23,18 @@
         });
     </script>
 
+    <script>
+        window.addEventListener('scroll', function() {
+            const navbar = document.querySelector('.navbar');
+            
+            if (window.scrollY > 10) {
+                navbar.classList.add('shadow-sm');
+            } else {
+                navbar.classList.remove('shadow-sm');
+            }
+        });
+    </script>
+
 </body>
 
 </html><?php /**PATH C:\Users\lanas\Documents\Codelabs\Laravel\personal-blog-laravel-10\resources\views/frontend/template/main.blade.php ENDPATH**/ ?>
