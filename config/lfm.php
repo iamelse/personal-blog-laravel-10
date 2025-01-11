8909c39751e673e46942f283c7c02d01146dba4a
Lana Septiana <lana.septiana2@gmail.com>
2025-01-11 16:56:12 +0700

    refactor: update .env.example and config/lfm.php * new variables added to .env.example to set disk and path for laravel file manager

--- config/lfm.php
+++ config/lfm.php
@@ -94,7 +94,7 @@ return [
     |--------------------------------------------------------------------------
      */
 
-    'disk'                     => 'idc_public_uploads', //env('FILESYSTEM_DISK'), //'public_uploads',
+    'disk'                     => env('LFM_DISK', 'public'), //env('FILESYSTEM_DISK'), //'public_uploads',
 
     'rename_file'              => false,
 
