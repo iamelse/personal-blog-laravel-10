6bd65ac19b945a6b374f62695b7d648d39ebe45d
Lana Septiana <lana.septiana2@gmail.com>
2025-03-01 12:21:38 +0700

    refresh database routes

--- routes/dev-idcloudhost.php
+++ routes/dev-idcloudhost.php
@@ -7,25 +7,9 @@ use Illuminate\Support\Facades\Schema;
 use Carbon\Carbon;
 
 Route::get('/refresh-database', function () {
-    $startTime = Carbon::now(); // Start time for execution tracking
-
-    // Get a list of all tables before migration
-    $oldTables = Schema::getAllTables();
-    
-    // Run migration fresh with seed
     Artisan::call('migrate:fresh --seed');
-
-    // Get a list of tables after migration
-    $newTables = Schema::getAllTables();
-    
-    // Get the tables that were created (difference between old and new)
-    $createdTables = array_diff(array_column($newTables, 'name'), array_column($oldTables, 'name'));
-
-    $executionTime = $startTime->diffInSeconds(Carbon::now()); // Calculate execution time
 
     return response()->json([
         'message' => 'Database refreshed successfully!',
-        'execution_time' => $executionTime . ' seconds',
-        'tables_created' => $createdTables,
     ]);
 });
